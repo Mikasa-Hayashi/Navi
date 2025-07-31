0b5de4af59cfec90b12a89aedcefa2cc1e529a36
Ivan <ivandenisovich2005@gmail.com>
2025-07-31 15:57:48 +0300

    chore: added user registration views and urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,5 +21,6 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/chat/', permanent=False)),
+    path('users/', include('users.urls')),
     path('chat/', include('chat.urls')),
 ]
