29a34078188b66743ef70da9b7ff2e7f6247d62f
Ivan <ivandenisovich2005@gmail.com>
2025-08-05 12:33:40 +0300

    chore: removed views from the core

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,7 +22,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/chat/', permanent=False)),
-    # path('', views.home_page),
     path('users/', include('users.urls')),
     path('chat/', include('chat.urls')),
     path('settings/', include('settings.urls')),
