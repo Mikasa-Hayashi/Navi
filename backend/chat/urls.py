c46f55cc43cead6545ab8bd2b6c38113e3884e0e
Ivan <ivandenisovich2005@gmail.com>
2025-08-07 10:01:41 +0300

    chore: added app name to urls refer

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'chat'
+
 urlpatterns = [
-    path('', views.conversation_list, name='conversations'),
-    path('<uuid:uuid>', views.conversation_detail, name='conversation'),
+    path('', views.conversation_list, name='conversation_list'),
+    path('<uuid:uuid>', views.conversation_detail, name='conversation_detail'),
 ]
