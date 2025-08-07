63e5b5ad7ee173f33b61db80b1643434284d09bb
Ivan <ivandenisovich2005@gmail.com>
2025-08-07 11:39:34 +0300

    chore: added custom registration form

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def register_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('chat:conversation_list')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', { 'form': form })
