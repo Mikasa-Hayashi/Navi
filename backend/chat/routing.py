2140e2798c5758af1586779a31a5814392754a85
Ivan <ivandenisovich2005@gmail.com>
2025-08-09 21:30:37 +0300

    chore: added consumers and routing for message sending in chat app

---
+++ backend/chat/routing.py
@@ -0,0 +1,6 @@
+from django.urls import re_path
+from .consumers import ChatConsumer
+
+websocket_urlpatterns = [
+    re_path(r'^ws/(?P<conversation_id>[0-9a-f-]{36})/send_message/$', ChatConsumer.as_asgi())
+]
