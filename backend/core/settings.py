d306897563f3dd4d5af4ba2c15c516f711d4dc76
Ivan <ivandenisovich2005@gmail.com>
2025-08-09 20:47:08 +0300

    chore: added channels and daphne app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,12 +45,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'daphne',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'users',
     'companion',
     'chat',
