24da73859aa0c954fc008d907614cba65aa06e1c
Ivan <ivandenisovich2005@gmail.com>
2025-08-10 13:17:53 +0300

    chore: added redis channel layers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -93,6 +93,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
 
 
+# Channel layers
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
