a65c1f448c6c72a096831605267ab2fdc40ce326
Ivan <ivandenisovich2005@gmail.com>
2025-08-10 15:50:37 +0300

    chore: added group

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -4,20 +4,38 @@ from asyncio import sleep
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
+        self.group_name = f'conversation_{self.conversation_id}' 
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name,
+        )
+
         await self.accept()
 
         await self.send(text_data=json.dumps({
             'type': 'connection_established',
-            'message': 'Hello, my creator! You are connected now!'
+            'message': 'Hello, my creator! You are connected now!',
         }))
     
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name,
+        )
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        for i in range(20):
-            await self.send(text_data=json.dumps({
-                'type': 'message',
-                'message': f'message #{i}'
-            }))
-            await sleep(0.5)
+        await self.channel_layer.group_send(self.group_name, {
+            'type': 'chat_message',
+            'message': message,
+        })
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'message',
+            'message': event['message'],
+        }))
