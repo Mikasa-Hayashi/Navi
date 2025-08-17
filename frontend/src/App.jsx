07d1014e05174b998d23741e445f05b893be058f
Ivan <ivandenisovich2005@gmail.com>
2025-08-17 14:25:49 +0300

    chore: added routers for chat and settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Chat from './pages/Chat';
+import Settings from './pages/Settings'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Chat />
+      <main className="main-content">
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </main>
     </>
   );
 }
---
+++ frontend/src/pages/Settings.jsx
@@ -0,0 +1,9 @@
+function Settings() {
+    return (
+        <div className="settings-page">
+            <p>There will be settings page</p>
+        </div>
+    );
+}
+
+export default Settings;
