fe5579fca4f5154f8fd42a105db1540834ccf68f
Ivan <ivandenisovich2005@gmail.com>
2025-08-20 11:28:24 +0300

    chore: added jwt authentication

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -176,7 +176,10 @@ LOGIN_URL = '/users/login/'
 
 # Rest framework
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ]
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
