7530622d5d072c97cfbd7ef8718dc1bb2a4ce0a5
Ivan <ivandenisovich2005@gmail.com>
2025-08-22 20:42:01 +0300

    chore: added user and password regex

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,3 +1,10 @@
+import { useRef, useState, useEffect } from 'react';
+
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3, 23}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8, 24}$/;
+
+
 function Register() {
     return (
         <div>
