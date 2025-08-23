7260d23588a1dfa801d2d81d2c9395998b3287ca
Ivan <ivandenisovich2005@gmail.com>
2025-08-23 21:33:05 +0300

    chore: added password fields

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -53,20 +53,53 @@ function Register() {
             <p ref={errorRef} className={errorMessage ? "error-message" : "offscreen"} aria-live="assertive">{errorMessage}</p>
             <h1>Sign up</h1>
             <form >
-                <label htmlFor="username">
-                    Username:
-                </label>
-                <input
-                    type="text"
-                    id="username"
-                    autoComplete="off"
-                    onChange={(event) => setUser(event.target.value)}
-                    required
-                    aria-invalid={validName ? "false" : "true"}
-                    aria-describedby="uidnote"
-                    onFocus={() => setUserFocus(true)}
-                    onBlur={() => setUserFocus(false)}
-                />
+                <div>
+                    <label htmlFor="username">
+                        Username:
+                    </label>
+                    <input
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(event) => setUser(event.target.value)}
+                        required
+                        aria-invalid={validName ? "false" : "true"}
+                        aria-describedby="uidnote"
+                        onFocus={() => setUserFocus(true)}
+                        onBlur={() => setUserFocus(false)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">
+                        Password:
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(event) => setPassword(event.target.value)}
+                        required
+                        aria-invalid={validPassword ? "false" : "true"}
+                        aria-describedby="pwdnote"
+                        onFocus={() => setPasswordFocus(true)}
+                        onBlur={() => setPasswordFocus(false)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirm-password">
+                        Confirm password:
+                    </label>
+                    <input
+                        type="password"
+                        id="confirm-password"
+                        onChange={(event) => setPassword(event.target.value)}
+                        required
+                        aria-invalid={validPassword ? "false" : "true"}
+                        aria-describedby="confirmnote"
+                        onFocus={() => setPasswordFocus(true)}
+                        onBlur={() => setPasswordFocus(false)}
+                    />
+                </div>
             </form>
         </section>
     )
