0d238c5f0f22ecac0d36ea5672b1651fb972fd09
Ivan <ivandenisovich2005@gmail.com>
2025-08-24 12:51:20 +0300

    chore: added create method in user serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -7,3 +7,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+    
+    def create(self, validated_data):
+        return User.objects.acreate(**validated_data)
