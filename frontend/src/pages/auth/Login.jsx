95256c2e3f6d0e588fd2d7368cf151423223a5c0
Ivan <ivandenisovich2005@gmail.com>
2025-08-25 19:18:20 +0300

    chore: added some hooks

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,4 +1,15 @@
+import { useRef, useState, useEffect } from 'react';
+
+
 function Login() {
+    const userRef = useRef();
+    const errorRef = useRef();
+    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [success, setSuccess] = useState(false);
+
     return (
         <div></div>
     )
