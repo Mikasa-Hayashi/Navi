a7ea35f2c6ab2f6b3f1a9abab5e5d9df7bb78c8c
Ivan <ivandenisovich2005@gmail.com>
2025-08-25 19:37:06 +0300

    chore: added success page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -18,42 +18,58 @@ function Login() {
         setErrorMessage('');
     }, [username, password])
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setSuccess(true);
+    }
+
     return (
-        <section>
-            <p ref={errorRef} className={errorMessage ? "error-message" : "offscreen"} aria-live="assertive">{errorMessage}</p>
-            <h1>Sign In</h1>
-            <form >
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input 
-                        type="text"
-                        id="username"
-                        ref={userRef}
-                        autoComplete="off"
-                        onChange={(event) => setUsername(event.target.value)}
-                        value={username}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input 
-                        type="password"
-                        id="password"
-                        onChange={(event) => setPassword(event.target.value)}
-                        value={password}
-                        required
-                    />
-                </div>
-                <button>Sign In</button>
-            </form>
-            <p>
-                Need an account?<br />
-                <span>
-                    <a href="#">Sign Up</a>
-                </span>
-            </p>
-        </section>
+        <>
+            {success ? (
+                <section>
+                    <h1>Success!</h1>
+                    <p>
+                        <a href="/chat">Chat</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errorRef} className={errorMessage ? "error-message" : "offscreen"} aria-live="assertive">{errorMessage}</p>
+                    <h1>Sign In</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="username">Username:</label>
+                            <input 
+                                type="text"
+                                id="username"
+                                ref={userRef}
+                                autoComplete="off"
+                                onChange={(event) => setUsername(event.target.value)}
+                                value={username}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password:</label>
+                            <input 
+                                type="password"
+                                id="password"
+                                onChange={(event) => setPassword(event.target.value)}
+                                value={password}
+                                required
+                            />
+                        </div>
+                        <button>Sign In</button>
+                    </form>
+                    <p>
+                        Need an account?<br />
+                        <span>
+                            <a href="#">Sign Up</a>
+                        </span>
+                    </p>
+                </section>
+            )}
+        </>
     )
 }
 
