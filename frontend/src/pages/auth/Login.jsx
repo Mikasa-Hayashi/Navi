e36ff14559752491b2992f4f8f89826fd3de2756
Ivan <ivandenisovich2005@gmail.com>
2025-08-25 19:26:18 +0300

    chore: added username field

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -19,7 +19,24 @@ function Login() {
     }, [username, password])
 
     return (
-        <div></div>
+        <section>
+            <p ref={errorRef} className={errorMessage ? "error-message" : "offscreen"} aria-live="assertive">{errorMessage}</p>
+            <h1>Sign In</h1>
+            <form >
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input 
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(event) => setUsername(event.target.value)}
+                        value={username}
+                        required
+                    />
+                </div>
+            </form>
+        </section>
     )
 }
 
