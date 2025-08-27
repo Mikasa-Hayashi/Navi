43fae29912345d62ee4e2d9b0dbc1017a37c8147
Ivan <ivandenisovich2005@gmail.com>
2025-08-27 10:52:01 +0300

    chore: added password to user serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'date_joined']
+        fields = ['username', 'password', 'date_joined']
     
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
