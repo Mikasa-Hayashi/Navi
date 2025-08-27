de4660b9f72c1c476d8794fac201e117532e9561
Ivan <ivandenisovich2005@gmail.com>
2025-08-27 19:58:47 +0300

    chore: removed `success` flag. Now redirecting where's we headed

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,11 +1,16 @@
 import { useRef, useState, useEffect } from 'react';
 import api from '../../services/api';
 import useAuth from '../../hooks/useAuth';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 const LOGIN_URL = '/api/v1/users/login/';
 
 function Login() {
     const { setAuth } = useAuth();
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
 
     const userRef = useRef();
     const errorRef = useRef();
@@ -13,7 +18,6 @@ function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
-    const [success, setSuccess] = useState(false);
 
     useEffect(() => {
         userRef.current.focus();
@@ -40,7 +44,8 @@ function Login() {
             setAuth({ username, password, accessToken });
             setUsername('');
             setPassword('');
-            setSuccess(true);
+            
+            navigate(from, { replace: true });
         } catch (error) {
             if (!error?.response) {
                 setErrorMessage('No Server Response');
@@ -56,52 +61,41 @@ function Login() {
     }
 
     return (
-        <>
-            {success ? (
-                <section>
-                    <h1>Success!</h1>
-                    <p>
-                        <a href="/chat">Chat</a>
-                    </p>
-                </section>
-            ) : (
-                <section>
-                    <p ref={errorRef} className={errorMessage ? "error-message" : "offscreen"} aria-live="assertive">{errorMessage}</p>
-                    <h1>Sign In</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <label htmlFor="username">Username:</label>
-                            <input 
-                                type="text"
-                                id="username"
-                                ref={userRef}
-                                autoComplete="off"
-                                onChange={(event) => setUsername(event.target.value)}
-                                value={username}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="password">Password:</label>
-                            <input 
-                                type="password"
-                                id="password"
-                                onChange={(event) => setPassword(event.target.value)}
-                                value={password}
-                                required
-                            />
-                        </div>
-                        <button>Sign In</button>
-                    </form>
-                    <p>
-                        Need an account?<br />
-                        <span>
-                            <a href="#">Sign Up</a>
-                        </span>
-                    </p>
-                </section>
-            )}
-        </>
+        <section>
+            <p ref={errorRef} className={errorMessage ? "error-message" : "offscreen"} aria-live="assertive">{errorMessage}</p>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input 
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(event) => setUsername(event.target.value)}
+                        value={username}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                        type="password"
+                        id="password"
+                        onChange={(event) => setPassword(event.target.value)}
+                        value={password}
+                        required
+                    />
+                </div>
+                <button>Sign In</button>
+            </form>
+            <p>
+                Need an account?<br />
+                <span>
+                    <a href="#">Sign Up</a>
+                </span>
+            </p>
+        </section>
     )
 }
 
