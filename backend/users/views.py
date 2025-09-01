0260dba7dd69fb3195b3acca2f16e66993aedd2a
Ivan <ivandenisovich2005@gmail.com>
2025-09-01 13:54:35 +0300

    chore: access token sends via json in the header now. Refresh token sends via httpOnly cookie

--- backend/users/views.py
+++ backend/users/views.py
@@ -30,10 +30,22 @@ class RegistrationView(APIView):
                 'date_joined': user.date_joined.isoformat(),
             })
 
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+
+            response = Response({
+                'accessToken': access_token,
             }, status=status.HTTP_201_CREATED)
+
+            response.set_cookie(
+                key='refreshToken',
+                value=refresh_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+            )
+
+            return response
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -67,16 +79,12 @@ class LoginView(APIView):
         access_token = str(refresh.access_token)
         refresh_token = str(refresh)
 
-        response = Response(status=status.http_200_OK)
+        response = Response({
+            'accessToken': access_token,
+        }, status=status.HTTP_200_OK)
+
         response.set_cookie(
-            key='access_token',
-            value=access_token,
-            httponly=True,
-            secure=True,
-            samesite='None',
-        )
-        response.set_cookie(
-            key='refresh_token',
+            key='refreshToken',
             value=refresh_token,
             httponly=True,
             secure=True,
@@ -95,16 +103,16 @@ class CookieTokenRefreshView(APIView):
 
     def post(self, request):
         print(request.COOKIES)
-        refresh_token = request.COOKIES.get('refresh_token')
+        refresh_token = request.COOKIES.get('refreshToken')
         if refresh_token is None:
             return Response({'error': 'Refresh token not provided;'}, status=status.HTTP_401_UNAUTHORIZED)
         try:
             refresh = RefreshToken(refresh_token)
             access_token = str(refresh.access_token)
 
-            response = Response(status=status.Htttp_200_OK)
+            response = Response(status=status.HTTP_200_OK)
             response.set_cookie(
-                key='access',
+                key='accessToken',
                 value=access_token,
                 httponly=True,
                 secure=True,
