f443563bd3472b5acec929a57c6ad33fb8fc35d8
Ivan <ivandenisovich2005@gmail.com>
2025-09-02 17:17:23 +0300

    chore: added temp link to the chat page

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 function Settings() {
     return (
         <div className="settings-page">
             <p>There will be settings page</p>
+            <Link to="/chat">Chat</Link>
         </div>
     );
 }
