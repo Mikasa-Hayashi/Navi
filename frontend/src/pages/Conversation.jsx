7250c5b2dc8ac0c639c12776ab3f08f96f758efe
Ivan <ivandenisovich2005@gmail.com>
2025-09-03 21:31:33 +0300

    chore: added messages fetching from backend

--- frontend/src/pages/Conversation.jsx
+++ frontend/src/pages/Conversation.jsx
@@ -1,11 +1,45 @@
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Message from '../components/Message';
+import { useState, useEffect } from 'react';
+import useAuth from '../hooks/useAuth';
+import { useNavigate, useLocation } from 'react-router-dom';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
 function Conversation() {
     const params = useParams();
+    const conversationId = params.uuid;
+    const { auth } = useAuth();
+    const axiosPrivate = useAxiosPrivate();
+    const [messages, setMessages] = useState([]);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        const fetchMessages = async () => {
+            try {
+                const response = await axiosPrivate.get(`/api/v1/chat/${conversationId}`, {
+                    'conversation_id': conversationId
+                });
+                setMessages(response.data);
+            } catch (error) {
+                console.error(error);
+                navigate('/login', { state: { from: location }, replace: true });
+            }
+        };
+
+        fetchMessages();
+    }, [])
 
     return (
         <div className="conversation-page">
-            <h2>Conversation { params.uuid }</h2>
+            <Link to="/chat">Back to chats</Link>
+            {/* <h2>Conversation { params.uuid }</h2> */}
+            <div className="message-container">
+                {messages.map(message => (
+                    <Message message={message} key={message.id} />
+                ))}
+            </div>
         </div>
     )
 }
