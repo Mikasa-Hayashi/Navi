c557c8963adc58fdb677c01bc8b04a957d4baafb
Ivan <ivandenisovich2005@gmail.com>
2025-09-03 21:10:34 +0300

    chore: remove `send_message` endpoint

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -5,6 +5,6 @@ app_name = 'chat'
 
 urlpatterns = [
     path('', views.ConversationList.as_view(), name='conversation_list'),
-    path('<uuid:conversation_id>/', views.MessageList.as_view(), name='conversation_detail'),
-    path('<uuid:conversation_id>/send_message', views.send_message, name='send_message'),
+    path('<uuid:conversation_id>', views.MessageList.as_view(), name='conversation_detail'),
+    # path('<uuid:conversation_id>/send_message', views.send_message, name='send_message'),
 ]
