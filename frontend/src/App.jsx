018291115742c6acffca47506d70a495eda96f1f
Ivan <ivandenisovich2005@gmail.com>
2025-09-20 21:13:45 +0300

    chore: disabled main css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
+// import './App.css';
 import Chat from './pages/Chat';
 import Settings from './pages/Settings'
 import BottomNavBar from './components/BottomNavBar';
