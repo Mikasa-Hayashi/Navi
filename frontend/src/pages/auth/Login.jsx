8416d612e20be4e737db12166b22fd838ae99370
Ivan <ivandenisovich2005@gmail.com>
2025-09-20 21:13:02 +0300

    chore: changed semantic for login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -2,6 +2,7 @@ import { useRef, useState, useEffect } from 'react';
 import axios from '../../api/axios';
 import useAuth from '../../hooks/useAuth';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+// import '../css/Login.css';
 
 const LOGIN_URL = '/api/v1/users/login/';
 
@@ -62,40 +63,70 @@ function Login() {
     }
 
     return (
-        <section>
-            <p ref={errorRef} className={errorMessage ? "error-message" : "offscreen"} aria-live="assertive">{errorMessage}</p>
-            <h1>Sign In</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="username">Username:</label>
+        <section className="login-section">
+            {/* Login header */}
+            <div className="form-header login-header">
+                <h1 className="form-title login-title">Sign In</h1>
+                <p ref={errorRef} className={errorMessage ? "error-message" : "offscreen"} aria-live="assertive">{errorMessage}</p>
+            </div>
+
+            {/* Login form */}
+            <form onSubmit={handleSubmit} className="form login-form">
+                {/* Username */}
+                <div className="form-group">
+                    <label htmlFor="username" className="form-label">Username</label>
                     <input 
                         type="text"
                         id="username"
+                        className="form-input"
+                        placeholder="Username"
                         ref={userRef}
                         autoComplete="off"
                         onChange={(event) => setUsername(event.target.value)}
                         value={username}
                         required
                     />
+                    <p></p>
                 </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
+
+                {/* Password */}
+                <div className="form-group">
+                    <label htmlFor="password" className="form-label">Password</label>
                     <input 
                         type="password"
                         id="password"
+                        className="form-input"
+                        placeholder="Password"
                         onChange={(event) => setPassword(event.target.value)}
                         value={password}
                         required
                     />
                 </div>
-                <button>Sign In</button>
+
+                {/* Remember me */}
+                <div className="form-group form-remember-me">
+                    <input type="checkbox" className="form-checkbox" />
+                    <label className="form-label">Remember me</label>
+                </div>
+
+                {/* Forgot password */}
+                <div className="form-group form-forgot-password">
+                    <Link to="#" className="form-link">Forgot password?</Link>
+                </div>
+                
+                {/* Submit */}
+                <div className="form-submit login-submit">
+                    <button type="submit" className="form-submit-button">Sign In</button>
+                </div>    
             </form>
-            <p>
-                Need an account?<br />
-                <span>
-                    <a href="#">Sign Up</a>
-                </span>
-            </p>
+
+            {/* Login footer */}
+            <div className="form-footer login-footer">
+                <p className="form-text register-text">
+                    Don't have an account?
+                    <Link to="#" className="form-link">Sign Up</Link>
+                </p>
+            </div>
         </section>
     )
 }
