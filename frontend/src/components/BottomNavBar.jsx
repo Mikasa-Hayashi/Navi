7932bdecf9f4b9164b643b61cf8cf6b166c4e35d
Ivan <ivandenisovich2005@gmail.com>
2025-09-22 19:07:29 +0300

    chore: redesigned navbar

--- frontend/src/components/BottomNavBar.jsx
+++ frontend/src/components/BottomNavBar.jsx
@@ -1,11 +1,48 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import '../css/BottomNavBar.css';
 
 function BottomNavBar() {
     return (
-        <nav className="bottom-navigation-bar">
-            <div>
-                <Link className="navbar-link" to="/chat">Chat</Link>
-                <Link className="navbar-link" to="/settings">Settings</Link>
+        <nav className="bottom-navbar">
+            <div className="bottom-navbar-item">
+                <NavLink id="chat" className="bottom-navbar-item-link" to="/chat">
+                    <div className="bottom-navbar-item-icon">
+
+                    </div>
+                    <span className="bottom-navbar-item-label">Chat</span>
+                </NavLink>    
+            </div>
+            <div className="bottom-navbar-item">
+                <NavLink id="model" className="bottom-navbar-item-link" to="/model">
+                    <div className="bottom-navbar-item-icon">
+
+                    </div>
+                    <span className="bottom-navbar-item-label">3d</span>
+                </NavLink>
+            </div>
+            <div className="bottom-navbar-item">
+                <NavLink id="companions" className="bottom-navbar-item-link" to="/companion">
+                    <div className="bottom-navbar-item-icon">
+
+                    </div>
+                    <span className="bottom-navbar-item-label">Companion</span>
+                </NavLink>
+            </div>
+            <div className="bottom-navbar-item">
+                <NavLink id="shop" className="bottom-navbar-item-link" to="/shop">
+                    <div className="bottom-navbar-item-icon">
+
+                    </div>
+                    <span className="bottom-navbar-item-label">Shop</span>
+                </NavLink>
+            </div>
+            <div className="bottom-navbar-item">
+                <NavLink id="settings" className="bottom-navbar-item-link" to="/settings">
+                    <div className="bottom-navbar-item-icon">
+
+                    </div>
+                    <span className="bottom-navbar-item-label">Settings</span>
+                </NavLink>
             </div>
         </nav>
     )
