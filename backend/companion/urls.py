6d288d07fb74dbe31dc8b02b2718a26b56ba8ae4
Ivan <ivandenisovich2005@gmail.com>
2025-09-22 20:59:07 +0300

    chore: added endpoint for geting companion by id

--- backend/companion/urls.py
+++ backend/companion/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'companion'
 
 urlpatterns = [
+    path('<int:companion_id>', views.CompanionDetailView.as_view(), name='companion_detail'),
     path('create/', views.create_companion, name='create'),
 ]
