a2f51903bbbc16a76bc564903595ac52d95a1dab
Ivan <ivandenisovich2005@gmail.com>
2025-09-22 21:02:48 +0300

    chore: added fetching companion

--- frontend/src/components/ConversationItem.jsx
+++ frontend/src/components/ConversationItem.jsx
@@ -1,15 +1,37 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
 function ConversationItem({conversation}) {
+    const axiosPrivate = useAxiosPrivate();
+    const [companion, setCompanion] = useState(null);
+    useEffect(() => {
+        const fetchCompanion = async () => {
+            try {
+                const response = await axiosPrivate.get(`api/v1/companion/${conversation.companion_id}`, {
+                });
+                setCompanion(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchCompanion();
+    }, [])
 
     return (
         <li className="conversation-item">
             <Link to={`/chat/${conversation.id}`}>
                 <div className="conversation-avatar">
-                    <img src={conversation.avatar} />
+                    {/* <img src={companion.avatar} /> */}
                 </div>
-                <div className="conversation-info">
+                <div className="conversation-title">
                     <h3 className="conversation-title">{conversation.title}</h3>
+                </div>
+                <div className="conversation-last-message">
+
+                </div>
+                <div className="conversation-timestamp">
+
                 </div>
             </Link>
         </li>
