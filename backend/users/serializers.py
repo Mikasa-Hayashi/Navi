7ef96fab455583cb49a4a9a027c302d44620d333
Ivan <ivandenisovich2005@gmail.com>
2026-05-22 12:35:47 +0300

    fix: add password matching validation to user serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -4,13 +4,21 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['username', 'password', 'date_joined']
+        fields = ['username', 'password', 'password2', 'date_joined']
         read_only_fields = ['date_joined']
         extra_kwargs = {
             'password': {'write_only': True},
         }
-    
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({'password2': 'Пароли не совпадают'})
+        return data
+
     def create(self, validated_data):
+        validated_data.pop('password2')
         return User.objects.create_user(**validated_data)
